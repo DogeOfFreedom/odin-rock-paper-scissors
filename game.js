77ceb6aebf6e1c5b38ee6646c15274daabf6be4a
DogeOfFreedom <jchensi2000@gmail.com>
2023-10-27 19:34:34 +1100

    feat: complete logic for a game round

--- game.js
+++ game.js
@@ -1,7 +1,34 @@
-let choices = ["rock", "paper", "scissors"];
+const CHOICES = ["rock", "paper", "scissors"];
+const WINNING_MATCHUPS = {
+    "rock" : "scissors",
+    "scissors" : "paper",
+    "paper" : "rock"
+}
 
 let getComputerChoice = () => {
     // Generate random number between 0-2
     let rand = Math.floor(Math.random() * 3);
-    return choices[rand];
+    return CHOICES[rand];
+}
+
+let play = () => {
+    let playerChoice = prompt("Enter your choice!").toLowerCase();
+    let computerChoice = getComputerChoice();
+    
+    // Draw
+    if(playerChoice === computerChoice) {
+        return "draw";
+    }
+    else {
+        // Check if the player won
+        let win_matchup = WINNING_MATCHUPS[playerChoice];
+        let playerWin = (win_matchup === computerChoice) 
+            ? true
+            : false;
+        
+        if(playerWin) {
+            return "player";
+        } 
+        return "cpu";
+    }
 }
